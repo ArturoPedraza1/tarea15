4e06b00764b570c04fa3499a43a865a0e32a1010
ArturoPedraza1 <125940774+ArturoPedraza1@users.noreply.github.com>
2023-05-12 18:30:25 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+import ico.Ventana;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Ventana ventana = new Ventana("Ventana");
+    }
+}
